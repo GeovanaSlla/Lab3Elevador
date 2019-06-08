2109b1fa7ff2369515ff908cc0d1279121d68b18
Gabriel Mendes Simoni <gmendes.simoni97@gmail.com>
2019-06-07 21:17:43 -0300

    Elevador
    Adicionados os arquivos fonte com a struct do elevador e as funções de avaliação em relação ao andar em que o elevador foi requisitado.

---
+++ blinky/src/elevador.h
@@ -0,0 +1,28 @@
+#ifndef _ELEVADOR_H_
+#define _ELEVADOR_H_
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#define ABERTA false
+#define FECHADA true
+#define DEVE_PARAR true
+#define NAO_DEVE_PARAR false
+#define NUM_ELEVADORES 3
+#define NUM_ANDARES 15
+
+typedef enum movimento_e {PARADO, DESCENDO, SUBINDO} movimento_e;
+
+typedef struct elevador_t
+{
+    uint8_t pos;
+    movimento_e mov;
+    bool porta;
+    bool paradas_subida[NUM_ANDARES + 1];
+    bool paradas_descida[NUM_ANDARES + 1];
+} elevador_t;
+#endif
+
+extern elevador_t vetorElevadores[NUM_ELEVADORES];
+uint8_t avaliaElevador(elevador_t* elevador, uint8_t posChamada);
